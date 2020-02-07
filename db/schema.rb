7b234053b5b5fdb813345444277666efeceee6fd
Ubuntu <ubuntu@ip-172-31-27-34.us-east-2.compute.internal>
2020-02-07 01:44:33 +0000

    Started building the database from the legacy code

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,66 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_31_011005) do
+ActiveRecord::Schema.define(version: 2020_02_07_005254) do
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
+  create_table "exercises", force: :cascade do |t|
+    t.string   "name"
+    t.string   "category"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+  end
+
+  add_index "exercises", ["user_id"], name: "index_exercises_on_user_id"
+
+  create_table "clients", force: :cascade do |t|
+    t.integer "trainer_user_id"
+    t.integer "client_user_id"
+  end
+
+  create_table "states", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+
+  create_table "tasks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "exercise_id"
+    t.integer "workout_id"
+    t.string "state"
+  end
+  
+  add_index "tasks", ["exercise_id"], name: "index_tasks_on_exercise_id"
+  add_index "tasks", ["workout_id"], name: "index_tasks_on_workout_id"
+
+  create_table "units", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "category"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
+    t.string "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "workouts", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "uid"
+    t.integer "user_id"
+    t.string "state"
+  end
+
+  add_index "workouts", ["user_id"], name: "index_workouts_on_user_id"
 
 end
