1baac62743ef9f56dc9f946e255a74ac2bad71c9
Ubuntu <ubuntu@ip-172-31-25-109.us-east-2.compute.internal>
2020-02-08 09:58:27 +0000

    fixed application.js to remove requirements for unneeded js files

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,12 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-require("packs/aos")
-require("packs/bootstrap.min")
-require("packs/custom")
-require("packs/jquery.min")
-require("packs/smoothscroll")
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
