e71305ce41d25ef2919156d8f742a5137b50316d
Ubuntu <ubuntu@ip-172-31-25-109.us-east-2.compute.internal>
2020-02-08 09:40:51 +0000

    workouts page started

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_31_011005) do
+ActiveRecord::Schema.define(version: 2020_02_08_051241) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "workouts", force: :cascade do |t|
+    t.string "wkname"
+    t.text "wkdesc"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
