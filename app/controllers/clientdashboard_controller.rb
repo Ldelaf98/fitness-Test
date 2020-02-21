d01ab35c08807482ed46ba57abe44cb1c618a082
Ubuntu <ubuntu@ip-172-31-24-118.us-east-2.compute.internal>
2020-02-21 01:26:05 +0000

    Started the basics aspects of setting goals

--- app/controllers/clientdashboard_controller.rb
+++ app/controllers/clientdashboard_controller.rb
@@ -1,5 +1,43 @@
 class ClientdashboardController < ApplicationController
   def index
     @current_user = current_user
+    @goals = Goals.all
+  end
+  
+  def show
+    @goal = Goal.find(params[:id])
+  end
+  def new
+      @goal = Goal.new
+  end
+  def edit
+    @goal = Goal.find(params[:id])
+  end
+  def create
+    @goal = Goal.new(goal_params)
+      if @goal.save
+        redirect_to @goal
+      else
+        render 'new'
+      end
+  end
+  def update
+    @goal = Goal.find(params[:id])
+      if @goal.update(goal_params)
+        redirect_to @goal
+      else
+        render 'edit'
+      end
+  end
+  def destroy
+    @goal = Goal.find(params[:id])
+    @goal.destroy
+        
+    redirect_to goals_path
+  end
+    
+private
+  def goal_params
+    params.require(:goal).permit(:title,:desc) 
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_08_051241) do
+ActiveRecord::Schema.define(version: 2020_02_21_000043) do
+
+  create_table "goals", force: :cascade do |t|
+    t.string "title"
+    t.text "desc"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
